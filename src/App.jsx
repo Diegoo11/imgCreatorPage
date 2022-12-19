e5493b4b42813f16ccdaf32b87322c890cdc263d
Diegoo11 <ynoacamino@gmail.com>
2022-12-19 11:38:44 -0500

    ajustes para la version movil

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { React } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <h1>Creador de imagenes</h1>
       <div className="card">
-        <input className='inputSearch' type="text"></input>
-        <button type="button" className='btnSearch'>
+        <input className="inputSearch" type="text" />
+        <button type="button" className="btnSearch">
           Generar
         </button>
         <p>
-          Creador de imagenes a traves de <code>OPENIA</code>
+          Creador de imagenes a traves de
+          {' '}
+          <code>OpenIA</code>
         </p>
+      </div>
+      <div className="boxImages">
+        <img className="imgGenered" src="https://play-lh.googleusercontent.com/J1edgzF2WiDMipbzea05rlmsn7WQxBX9APPoMYmE114ifAXZsP2pjBiwde6637jgBHU" alt="" />
+        <img className="imgGenered" src="https://play-lh.googleusercontent.com/w0XdIeOEgQJxuldR61Y8Pr8vFuC_u-CVTRLStS2vnYeauap5sktn1hykpk2hp2t4BA" alt="" />
+        <img className="imgGenered" src="https://i.pinimg.com/736x/b8/70/2f/b8702f30ceb79cf719cd41c8275211df.jpg" alt="" />
+        <img className="imgGenered" src="https://play-lh.googleusercontent.com/bb_VXt7sHeSwmfYsWMGF1mMdw_XQ9Wo880h59tp2PNZjSCuzDjPfbsDS9m2OA3Oraso" alt="" />
+        <img className="imgGenered" src="https://image.winudf.com/v2/image1/Y29tLml3YW5yZXdvay5DYXR3YWxscGFwZXJzX3NjcmVlbl8wXzE1NjcxNDE1MzJfMDY5/screen-0.jpg?fakeurl=1&type=.webp" alt="" />
       </div>
       <p className="read-the-docs">
         For Diegoo11
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
